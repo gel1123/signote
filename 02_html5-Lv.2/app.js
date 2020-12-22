013791879b5e917b0e7e97c0b3d394448501a381
gel1123 <atno.14@gmail.com>
2020-12-22 09:16:10 +0900

    studying...

--- 02_html5-Lv.2/app.js
+++ 02_html5-Lv.2/app.js
@@ -7,15 +7,39 @@ server.on('request', doRequest);
 var fs = require('fs');
 function doRequest(req, res) {
     console.log(new Date() + " : " + req.url);
-    fs.readFile('./test.html', 'utf-8' , doRead );
-    function doRead(err, data) {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write(data);
+    if (req.url == "/xhr") {
+        let body = "";
+        req.on("data", chunk => body += chunk);
+        req.on("end", () => {
+            console.log(body);
+	});
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write("received xhr.");
         res.end();
+    } else {
+        fs.readFile('./test.html', 'utf-8' , (err, data) => {
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.write(data);
+            res.end();
+        });
     }
-    
 }
  
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+// web socket
+let ws = require('ws')
+var wsserver = new ws.Server({port:8000});
+wsserver.on('connection', ws => {
+    ws.on('message', message => {
+        console.log(message);
+        wsserver.clients.forEach(client => {
+            client.send("received a message : '"+message+"'");
+        });
+    });
+    ws.on('close', () => {
+        console.log('close');
+    });
 });
