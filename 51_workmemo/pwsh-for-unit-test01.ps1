e6194ad3a108d50929ef0cca05d6352a5cef1367
gel1123 <atno.14@gmail.com>
2021-08-05 08:22:07 +0900

    hoge

--- 51_workmemo/pwsh-for-unit-test01.ps1
+++ 51_workmemo/pwsh-for-unit-test01.ps1
@@ -1,6 +1,8 @@
 $obj = @{ num = 1  };
 
-function n() { Write-Host "now : $($obj.num.ToString('000'))" -ForegroundColor Blue ;  $obj.num = $obj.num+1; mkdir $obj.num.ToString('000'); Move-Item *.png,*.json,*.log -Destination $obj.num.ToString('000'); ls $obj.num.ToString('000');  Write-Host "next: $($obj.num.ToString('000'))" -ForegroundColor Green;}
+function add-func() { Write-Host "dummy"; }
+
+function n() { Write-Host "now : $($obj.num.ToString('000'))" -ForegroundColor Blue ;  $obj.num = $obj.num+1; mkdir $obj.num.ToString('000'); add-func; Move-Item *.png,*.json,*.log -Destination $obj.num.ToString('000'); ls $obj.num.ToString('000');  Write-Host "next: $($obj.num.ToString('000'))" -ForegroundColor Green;}
 
 function reset() { Write-Host "now : $($obj.num.ToString('000'))" -ForegroundColor Blue ;  $obj.num = 1;  Write-Host "next: $($obj.num.ToString('000'))" -ForegroundColor Green;}
 
